682c462a50a5824532c9e7531645b30a6338a857
Saggi Mizrahi <ficoos@gmail.com>
2015-01-23 12:18:02 +0200

    Initial commit
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

---
+++ conf.lua
@@ -0,0 +1,28 @@
+function love.conf(t)
+    t.author = "Spoop Team"
+    t.identity = "spoop"
+    t.version = "0.9.1"
+
+    t.window.title = "Space Poop"
+    t.window.fullscreen = false
+    t.window.vsync = false
+    t.window.fsaa = 1
+    t.window.width = 1280
+    t.window.height = 720
+    t.window.resizable = true
+    t.window.minwidth = 300
+    t.window.minheight = 300
+
+    -- modules
+    t.modules.joystick = true
+    t.modules.audio = true
+    t.modules.keyboard = true
+    t.modules.event = true
+    t.modules.image = true
+    t.modules.graphics = true
+    t.modules.timer = true
+    t.modules.mouse = true
+    t.modules.sound = true
+    t.modules.thread = false
+    t.modules.physics = true
+end
---
+++ main.lua
@@ -0,0 +1,5 @@
+local lg = love.graphics
+
+function love.draw()
+    lg.print("Space Poop!!!", 200, 200)
+end
