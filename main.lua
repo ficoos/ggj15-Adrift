264b1660d27780435f8f2519ee8a6b42277dce50
Saggi Mizrahi <ficoos@gmail.com>
2015-01-23 15:24:41 +0200

    Basic movement
    Signed-off-by: Saggi Mizrahi <ficoos@gmail.com>

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ function love.load()
 end
 
 function love.update(dt)
-    GameState:update(dt)
+    GameState.update(dt)
 end
 
 function love.draw()
--- states/SpaceRescue.lua
+++ states/SpaceRescue.lua
@@ -1,9 +1,47 @@
+local Astronaut = require 'Astronaut'
+
 local lg = love.graphics
+local lp = love.physics
+local lm = love.mouse
+local lw = love.window
 
 local SpaceRescue = {}
 
+local FORCE = 10000
+
+function SpaceRescue:enter(prev, ...)
+    lp.setMeter(2)
+    self._world = lp.newWorld(0, 0, true)
+    self._astronaut = Astronaut(self._world)
+    self._drawables = {{self._astronaut}}
+    self._astronaut:set_position(lw.getWidth()/2, lw.getHeight()/2)
+end
+
+function SpaceRescue:_push_player()
+    local x, y = lm.getPosition()
+    local ax, ay = self._astronaut:get_position()
+    local theta = math.atan2(x - ax, ay -y) + (0.5 * math.pi)
+
+    local ix = -math.cos(theta) * FORCE
+    local iy = -math.sin(theta) * FORCE
+    self._astronaut:apply_force(ix, iy)
+end
+
+function SpaceRescue:update(dt)
+    self._world:update(dt)
+    if lm.isDown("l") then
+        self:_push_player()
+    end
+end
+
 function SpaceRescue:draw()
-    lg.print("Space Poop", 200, 200)
+    for _, layer in ipairs(self._drawables) do
+        for _, obj in ipairs(layer) do
+            lg.push()
+                obj:draw()
+            lg.pop()
+        end
+    end
 end
 
 function SpaceRescue:keypressed(key)
